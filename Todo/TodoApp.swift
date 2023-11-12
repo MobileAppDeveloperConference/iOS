c6ac17e5ed3f4507ea97ea5619492d59256eaeda
상선 <hiddenparts@gmail.com>
2023-11-12 14:51:16 +0900

    first commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by 상선 on 11/12/23.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
