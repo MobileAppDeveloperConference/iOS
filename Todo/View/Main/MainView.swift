018a954948f4026650d1ca8e13bdf0e219b99cdb
상선 <hiddenparts@gmail.com>
2023-11-15 02:30:28 +0900

    [#11] 기본 탭에서 완료숨기기 기능 구현하기

--- Todo/View/Main/MainView.swift
+++ Todo/View/Main/MainView.swift
@@ -9,9 +9,14 @@ import SwiftUI
 
 struct MainView: View {
     @Environment(TaskManager.self) var taskManager
+    @State private var isShowDoneTask: Bool = true
     
     var tasks: [TaskModel] { taskManager.tasks.filter { $0.isDone == false } }
     var doneTasks: [TaskModel] {taskManager.tasks.filter { $0.isDone == true } }
+    
+    var isShowDontTaskButtonTitle: String {
+        isShowDoneTask ? "완료 숨기기" : "완료 보기"
+    }
     
     var body: some View {
         @Bindable var taskManager = taskManager
@@ -19,7 +24,7 @@ struct MainView: View {
         NavigationStack {
             List {
                 Section {
-                    if taskManager.tasks.isEmpty {
+                    if tasks.isEmpty {
                         TaskEmptyView()
                     } else {
                         ForEach(tasks) {
@@ -36,17 +41,19 @@ struct MainView: View {
                     Text("하는 중")
                 }
                 
-                Section {
-                    ForEach(doneTasks) {
-                        TaskListItemView(task: $0)
-                            .swipeActions {
-                                Button("삭제") {
-                                    
-                                }.tint(Color.Todo.red)
-                            }
+                if isShowDoneTask {
+                    Section {
+                        ForEach(doneTasks) {
+                            TaskListItemView(task: $0)
+                                .swipeActions {
+                                    Button("삭제") {
+                                        
+                                    }.tint(Color.Todo.red)
+                                }
+                        }
+                    } header: {
+                        Text("완료")
                     }
-                } header: {
-                    Text("완료")
                 }
             }
             .buttonStyle(.borderless) // 리스트에서 아이템을 탭했을때 모든 버튼들이 이벤트를 동시에 받는 것을 방지
@@ -57,8 +64,8 @@ struct MainView: View {
                         .foregroundStyle(Color.Todo.black)
                 }
                 ToolbarItem(placement: .topBarTrailing) {
-                    Button("완료 숨기기") {
-                        
+                    Button(isShowDontTaskButtonTitle) {
+                        isShowDoneTask.toggle()
                     }
                 }
             }
